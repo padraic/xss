dddc05bdefb3fb2439544e5c9066ce8ff6caa6cf
Pádraic Brady <padraic.brady@gmail.com>
2012-03-03 20:44:50 +0000

    Added initial examples

---
+++ examples/2-htmlspecialchars-quoteless_attribute-fixed.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+
+$input = <<<INPUT
+faketitle onmouseover=alert(/Meow!/);
+INPUT;
+$output = htmlspecialchars($input, ENT_QUOTES);
+
+?>
+<html>
+<head>
+    <title>Single Quoted Attribute</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+    <div>
+        <span title="<?= $output ?>">What's that latin placeholder text again?</span>
+    </div>
+</body>
+</html>
