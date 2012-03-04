551820574ad7398accf28b39864f51612638ce7c
Pádraic Brady <padraic.brady@gmail.com>
2012-03-04 12:24:29 +0000

    Added second example for swallowing quotes

---
+++ examples/3-htmlspecialchars-swallowed_quotes2-fixed.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+$input1 = 'fakeimage'."\xC0";
+$input2 = <<<INPUT2
+onerror=alert(/Meow!/)//
+INPUT2;
+$output1 = htmlspecialchars($input1, ENT_QUOTES, 'UTF-8');
+$output2 = htmlspecialchars($input2, ENT_QUOTES, 'UTF-8');
+?>
+<html>
+<head>
+    <title>Swallowed Quotes</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+    <div>
+        <img src="http://example.com/images/<?= $output1 ?>"
+        title="<?= $output2 ?>">
+    </div>
+</body>
+</html>
